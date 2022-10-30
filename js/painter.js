410d277ee4fa69257b9c3ad48ea17ef344a9a1b4
konfesor <Maksim.Kibanov@urfu.me>
2022-10-31 02:45:38 +0500

    created module painter

---
+++ js/painter.js
@@ -0,0 +1,16 @@
+import {createPhotoDescriptions} from './data.js';
+
+const pictures = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture');
+
+for (const photoDescription of createPhotoDescriptions) {
+  const photo = pictureTemplate.cloneNode(true);
+  const image = photo.querySelector('.picture__img');
+  const likes = photo.querySelector('.picture__likes');
+  const comments = photo.querySelector('.picture__comments');
+  image.src = photoDescription.url;
+  likes.textContent = photoDescription.likes;
+  comments.textContent = photoDescription.comments;
+
+  pictures.appendChild(photo);
+}
