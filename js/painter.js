1ed24205e2a19d3e7fe010606c491634a9d34eb0
konfesor <Maksim.Kibanov@urfu.me>
2022-10-31 02:49:41 +0500

    changed import file name

--- js/painter.js
+++ js/painter.js
@@ -1,9 +1,9 @@
-import {createPhotoDescriptions} from './data.js';
+import {PhotoDescriptions} from './data.js';
 
 const pictures = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture');
 
-for (const photoDescription of createPhotoDescriptions) {
+for (const photoDescription of PhotoDescriptions) {
   const photo = pictureTemplate.cloneNode(true);
   const image = photo.querySelector('.picture__img');
   const likes = photo.querySelector('.picture__likes');
