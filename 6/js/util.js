8cdaf22508ed741a33ceb69ed9a028b19ae89891
Keks <keks@htmlacademy.ru>
2022-11-15 19:47:43 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/util.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function isCorrectLength(string, maxLength) {
+  return string.length <= maxLength;
+}
+
+export{getRandomPositiveInteger, isCorrectLength};
