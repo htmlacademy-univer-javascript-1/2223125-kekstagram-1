87531f6f8f1df8b3a19aab6f48ba522ebf69adc8
konfesor <Maksim.Kibanov@urfu.me>
2022-11-22 15:51:33 +0500

    created comments upload button

--- js/render.js
+++ js/render.js
@@ -1,4 +1,4 @@
-import {PhotoDescriptions} from './data.js';
+import {createPhotoDescriptions} from './data.js';
 import {openBigPhoto} from './fullScreen.js';
 
 const picturesContainerElement = document.querySelector('.pictures');
@@ -7,14 +7,14 @@ const pictureTemplate = document.querySelector('#picture');
 const renderPhotos = () => {
   const newListFragment = document.createDocumentFragment();
 
-  for (const photoDescription of PhotoDescriptions) {
+  for (const photoDescription of createPhotoDescriptions()) {
     const photo = pictureTemplate.cloneNode(true);
     const image = photo.querySelector('.picture__img');
     const likes = photo.querySelector('.picture__likes');
     const comments = photo.querySelector('.picture__comments');
     image.src = photoDescription.url;
     likes.textContent = photoDescription.likes;
-    comments.textContent = photoDescription.comments;
+    comments.textContent = photoDescription.comments[0];
 
     photo.addEventListener('click', () => {
       openBigPhoto(photo);
