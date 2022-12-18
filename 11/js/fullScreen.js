ba09a9600dd866044b9b8723426c12015e7fdd6e
Keks <keks@htmlacademy.ru>
2022-12-18 18:54:49 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/fullScreen.js
@@ -0,0 +1,75 @@
+import {getRandomPositiveInteger} from './util.js';
+
+const bigPictureSection = document.querySelector('.big-picture');
+const img = bigPictureSection.querySelector('.big-picture__img').querySelector('img');
+const likes = bigPictureSection.querySelector('.likes-count');
+const description = bigPictureSection.querySelector('.social__caption');
+const commentsCount = bigPictureSection.querySelector('.comments-count');
+const commentsList = bigPictureSection.querySelector('.social__comments');
+const currentCommentsCount = bigPictureSection.querySelector('.current-comments-count');
+const loadCommentsButton = bigPictureSection.querySelector('.social__comments-loader');
+const authorAvatar = bigPictureSection.querySelector('.social__header').querySelector('img');
+
+const addComment = (photo) => {
+  const currentComment = photo.comments[Number(currentCommentsCount.textContent)];
+  commentsList.insertAdjacentHTML('beforeend', `
+  <li class="social__comment">
+    <img class="social__picture" src="${currentComment.avatar}" alt="${currentComment.name}" width="35" height="35">
+    <p class="social__text">"${currentComment.message}"</p>
+  </li>`);
+  currentCommentsCount.textContent = Number(currentCommentsCount.textContent) + 1;
+};
+
+const loadFiveComments = (photo) => {
+  for (let i = 0; i < 5; i++) {
+    if (Number(currentCommentsCount.textContent) < Number(commentsCount.textContent)) {
+      addComment(photo);
+    }
+    else {
+      loadCommentsButton.classList.add('hidden');
+      break;
+    }
+  }
+  if (Number(currentCommentsCount.textContent) >= Number(commentsCount.textContent)) {
+    loadCommentsButton.classList.add('hidden');
+  }
+};
+
+const openBigPhoto = (photo) => {
+  bigPictureSection.classList.remove('hidden');
+  commentsCount.textContent = photo.comments.length;
+  authorAvatar.src = `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`;
+
+  loadFiveComments(photo);
+
+  loadCommentsButton.onclick = () => {
+    loadFiveComments(photo);
+  };
+
+  img.src = photo.url;
+  likes.textContent = photo.likes;
+  description.textContent = photo.description;
+
+  document.querySelector('body').classList.add('modal-open') ;
+
+  const closeButton = bigPictureSection.querySelector('.big-picture__cancel');
+  closeButton.addEventListener('click', () => {
+    bigPictureSection.classList.add('hidden');
+    document.querySelector('body').classList.remove('modal-open');
+    currentCommentsCount.textContent = 0;
+    commentsList.textContent = '';
+    loadCommentsButton.classList.remove('hidden');
+  });
+
+  document.addEventListener('keydown', (evt) => {
+    if (evt.keyCode === 27) {
+      bigPictureSection.classList.add('hidden');
+      document.querySelector('body').classList.remove('modal-open');
+      currentCommentsCount.textContent = 0;
+      commentsList.textContent = '';
+      loadCommentsButton.classList.remove('hidden');
+    }
+  });
+};
+
+export {openBigPhoto};
